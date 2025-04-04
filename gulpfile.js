34cc6a31b212288ef828fcad0d36712d972186ae
Alejandro <arobles@uniempresarial.edu.co>
2025-04-03 23:06:37 -0500

    Habilitado watch con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
-export function hola(done){
-    console.log('Hola Mundo');
-    done();
+import {src, dest, watch} from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass);
+
+export function css( done ) {
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('dist/css') )
+
+    done()
 }
+export function dev(){
+    watch('src/scss/app.scss', css)
+}
+
